0608b21b9965af0c16add5914399e6ddee20c374
Srdjan Rakic <srdjanr.1609@gmail.com>
2023-05-26 20:05:09 +0200

    added usestate test

--- packages/shared-ui/Button.tsx
+++ packages/shared-ui/Button.tsx
@@ -5,6 +5,16 @@ export function Button(props: {
   onClick: () => void;
   children: React.ReactNode;
 }) {
-  return <button onClick={() => props.onClick()}>{props.children}</button>;
+  const [count, setCount] = React.useState(0);
+  return (
+    <button
+      onClick={() => {
+        setCount(count + 1);
+        props.onClick();
+      }}
+    >
+      {props.children} {count}
+    </button>
+  );
 }
 export default Button;
